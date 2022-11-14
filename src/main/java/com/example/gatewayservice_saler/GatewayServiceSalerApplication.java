9a6a88fb10a5593dab10d53f67dd64e28601cf47
ImKunYoung <ur2kunyoung2@outlook.com>
2022-11-14 23:18:43 +0900

    init commit

---
+++ src/main/java/com/example/gatewayservice_saler/GatewayServiceSalerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayservice_saler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceSalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceSalerApplication.class, args);
+    }
+
+}
